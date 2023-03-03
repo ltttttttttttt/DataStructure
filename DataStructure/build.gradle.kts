f61e6567bb36a1421d15a2663d0d5cc58d1895d2
lt_taozi <1764780379@qq.com>
2023-03-03 16:42:14 +0800

    支持kmp全平台(未完成)

---
+++ DataStructure/build.gradle.kts
@@ -0,0 +1,131 @@
+/*
+ * Copyright lt 2022
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("convention.publication")
+//    kotlin("native.cocoapods")
+}
+
+group = "io.github.ltttttttttttt"
+//上传到mavenCentral命令: ./gradlew publishAllPublicationsToSonatypeRepository
+//mavenCentral后台: https://s01.oss.sonatype.org/#stagingRepositories
+version = "1.0.8"
+
+kotlin {
+    android {
+        publishLibraryVariants("debug", "release")
+    }
+
+    jvm("desktop") {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+    }
+
+    ios()
+    iosSimulatorArm64()
+
+    js(IR) {
+        browser()
+    }
+
+//    macosX64 {
+//        binaries {
+//            executable {
+//                entryPoint = "main"
+//            }
+//        }
+//    }
+//    macosArm64 {
+//        binaries {
+//            executable {
+//                entryPoint = "main"
+//            }
+//        }
+//    }
+//
+//    cocoapods {
+//        summary = "Jatpack(JetBrains) Compose views"
+//        homepage = "https://github.com/ltttttttttttt/ComposeViews"
+//        ios.deploymentTarget = "14.1"
+//        podfile = project.file("../ios_app/Podfile")
+//        framework {
+//            baseName = "ComposeViews"
+//            isStatic = true
+//        }
+//        extraSpecAttributes["resources"] = "['src/commonMain/resources/**', 'src/desktopMain/resources/**', 'src/iosMain/resources/**']"
+//    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //kotlin
+                implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+                //协程
+                implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val androidMain by getting
+        val androidTest by getting
+//        val androidUnitTest by getting
+
+        val desktopMain by getting
+        val desktopTest by getting
+
+        val iosMain by getting
+        val iosTest by getting
+        val iosSimulatorArm64Main by getting {
+            dependsOn(iosMain)
+        }
+        val iosSimulatorArm64Test by getting {
+            dependsOn(iosTest)
+        }
+
+//        val macosMain by creating {
+//            dependsOn(commonMain)
+//        }
+//        val macosX64Main by getting {
+//            dependsOn(macosMain)
+//        }
+//        val macosArm64Main by getting {
+//            dependsOn(macosMain)
+//        }
+    }
+}
+
+android {
+    compileSdk = 33
+    namespace="com.lt.data_structure"
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 31
+        sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
--- app/src/main/java/com/lt/app/MainActivity.kt
+++ app/src/main/java/com/lt/app/MainActivity.kt
@@ -14,9 +14,17 @@
  * limitations under the License.
  */
 
+package com.lt.app
 
-rootProject.name = "DataStructure"
-include("app")
-include("DataStructure")
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.lt.data_structure.basic_value.IntArrayList
 
-includeBuild("convention-plugins")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.tv).text = IntArrayList(intArrayOf(1, 2, 3, 4, 5)).toString()
+    }
+}
